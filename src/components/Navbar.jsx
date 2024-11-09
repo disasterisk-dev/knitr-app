d473ebffb5813f0ac3b3eb2fba008336e89ca360
Joel Robinson <joelsgrobinson@gmail.com>
2024-11-09 18:07:45 +0000

    Made Navbar display buttons/links dynamically based on router location

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,17 +1,34 @@
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faPlus } from "@fortawesome/free-solid-svg-icons";
-import { Link } from "react-router-dom";
+import { faPlus, faTrash } from "@fortawesome/free-solid-svg-icons";
+import { Link, useLocation, useNavigate } from "react-router-dom";
 
 const Navbar = () => {
+  const { pathname } = useLocation();
+  const navigate = useNavigate();
+
+  function handleCancelCreate(e) {
+    e.preventDefault();
+    let check = confirm("Do you want to discard this project?");
+
+    if (check) navigate("/");
+  }
+
   return (
-    <header className="flex px-8 py-4 bg-brand-300 text-inverse sticky top-0">
+    <header className="sticky top-0 flex bg-brand-300 px-8 py-4 text-inverse">
       <Link to={"/"} className="grow">
         <h1 className="font-brand text-4xl font-black">Knitr</h1>
       </Link>
-      <nav className="flex justify-center items-center text-3xl">
-        <Link to={"/create"}>
-          <FontAwesomeIcon icon={faPlus} />
-        </Link>
+      <nav className="flex items-center justify-center text-3xl">
+        {pathname === "/" && (
+          <Link to={"/create"}>
+            <FontAwesomeIcon icon={faPlus} />
+          </Link>
+        )}
+        {pathname === "/create" && (
+          <button onClick={handleCancelCreate}>
+            <FontAwesomeIcon icon={faTrash} />
+          </button>
+        )}
       </nav>
     </header>
   );
