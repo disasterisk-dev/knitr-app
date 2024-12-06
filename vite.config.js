7444b7b52521183e990b51ad9abccb1617b53f32
JoelRobinsonUK <joelsgrobinson@gmail.com>
2024-12-06 18:01:08 +0000

    Set up PWA config

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,32 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import { VitePWA } from "vite-plugin-pwa";
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+  plugins: [
+    react(),
+    VitePWA({
+      registerType: "autoUpdate",
+      includeAssets: ["knitr.svg", "knitr-180.png"],
+      manifest: {
+        name: "Knitr",
+        short_name: "Knitr",
+        description: "A knitting project management app",
+        theme_color: "#D46B80",
+        icons: [
+          {
+            src: "knitr-192x192.png",
+            sizes: "192x192",
+            type: "image/png",
+          },
+          {
+            src: "knitr-512x512.png",
+            sizes: "512x512",
+            type: "image/png",
+          },
+        ],
+      },
+    }),
+  ],
+});
